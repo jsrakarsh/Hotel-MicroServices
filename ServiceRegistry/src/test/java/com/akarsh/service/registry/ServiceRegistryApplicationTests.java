d84be6daca1e08d02859e389eba2c72fbaa15676
Akarsh Singh <jsrakarsh@gmail.com>
2025-08-31 23:31:20 +0530

    Added Eureka Server and registered Hotel, Rating and User services

---
+++ ServiceRegistry/src/test/java/com/akarsh/service/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akarsh.service.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- UserService/src/main/java/com/akarsh/user/service/UserServiceApplication.java
+++ UserService/src/main/java/com/akarsh/user/service/UserServiceApplication.java
@@ -1,13 +1,16 @@
+
 package com.akarsh.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class UserServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
 
 }
