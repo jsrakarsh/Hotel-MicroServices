7c2346e083db78fd090e83cb3721c307a7991920
Akarsh Singh <jsrakarsh@gmail.com>
2025-09-04 09:00:31 +0530

    Implemented API Gateway for centralized routing and load balancing across microservices

---
+++ ServiceRegistry/ApiGateway/src/test/java/com/akarsh/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akarsh.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- UserService/src/main/java/com/akarsh/user/service/external/services/RatingService.java
+++ UserService/src/main/java/com/akarsh/user/service/external/services/RatingService.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "RATINGSERVICE")
+@FeignClient(name = "RATING-SERVICE")
 public interface RatingService  {
 
     @GetMapping("/ratings/user/{userId}")
