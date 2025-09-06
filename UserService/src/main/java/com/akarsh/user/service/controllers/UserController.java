cff84e839124162100746168f5a2422423a9348b
Akarsh Singh <jsrakarsh@gmail.com>
2025-09-06 20:56:31 +0530

    Implemented Circuit Breaker using Resilience4j to handle service failures gracefully

--- UserService/src/main/java/com/akarsh/user/service/controllers/UserController.java
+++ UserService/src/main/java/com/akarsh/user/service/controllers/UserController.java
@@ -1,12 +1,15 @@
 package com.akarsh.user.service.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.akarsh.user.service.entities.User;
 import com.akarsh.user.service.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -16,6 +19,8 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
 
     // CREATE USER
@@ -29,10 +34,30 @@ public class UserController {
 
     // GET SINGLE USER
     @GetMapping("/{userId}")
+    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId) {
         User user = userService.getUser(userId);
         return ResponseEntity.ok(user);
     }
+
+    // Creating fallback method for Circuit Breaker
+
+    // the fallback method must have the same return type and parameters as the original method, with an additional parameter for the exception
+    // here the original method is getSingleUser which returns ResponseEntity<User> and takes String userId as parameter
+    // so the fallback method must return ResponseEntity<User> and take String
+    public ResponseEntity<User> ratingHotelFallback(String userId, Exception ex) {
+        logger.info("Fallback is executed because service is down: {}", ex.getMessage());
+
+        User user = User.builder()
+                .email("dummy@gmail.com")
+                .name("Dummy")
+                .about("This user is created as a dummy because some service is down")
+                .userId("141234")
+                .build();
+
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
 
 
 
